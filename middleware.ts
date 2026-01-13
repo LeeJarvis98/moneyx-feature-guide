11a28191f6cb6b46516757d128c4eddb9caec846
LeeJarvis98 <leejarvis98@users.noreply.github.com>
2026-01-13 09:21:38 +0700

    feat: add middleware for internationalization with locale detection disabled

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 ﻿import createMiddleware from 'next-intl/middleware';
 import { routing } from './routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+  ...routing,
+  // Disable automatic locale detection based on browser language
+  localeDetection: false
+});
 
 export const config = {
   // Match only internationalized pathnames
